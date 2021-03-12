443c512494034db1ff64185410da3e6ef8f1c184
szabolevi98 <46320470+szabolevi98@users.noreply.github.com>
2021-03-12 18:28:50 +0100

    2021.03.12.

--- routes/index.js
+++ routes/index.js
@@ -15,38 +15,55 @@ router.route('/')
   });
 })
 .post(async(req, res) => {
-  const articleObject = {
-      name: req.body.name,
-      description: req.body.description,
-      full: req.body.full.replace(/\r\n/g, '<br />')
-  };
-  if (articleObject.name && articleObject.description && articleObject.full) {
-      try {
-          const postArticle = new articleModel(articleObject);
-          await postArticle.save();
-          res.render('index', { 
-            layout: path.join('layouts', 'with_jumbotron'),
-            title: 'Home',
-            nav: '1',
-            articles: await articleModel.find().lean(),
-            createArticle: true,
-            articleSuccess: true
-          });
-      } 
-      catch (err) {
-          res.render('error', { 
-              title: 'Error',
+  if (req.body.password == process.env.SECRET_KEY) {
+    const articleObject = {
+        name: req.body.name,
+        description: req.body.description,
+        full: req.body.full.replace(/\r\n/g, '<br />')
+    };
+    if (articleObject.name && articleObject.description && articleObject.full) {
+        try {
+            const postArticle = new articleModel(articleObject);
+            await postArticle.save();
+            res.render('index', { 
+              layout: path.join('layouts', 'with_jumbotron'),
+              title: 'Home',
               nav: '1',
-              specificMessage: err.message
-          });
-      }
+              articles: await articleModel.find().lean(),
+              createArticle: true,
+              articleSuccess: true
+            });
+        } 
+        catch (err) {
+            res.render('error', { 
+                title: 'Error',
+                nav: '1',
+                specificMessage: err.message
+            });
+        }
+    }
+    else {
+        res.render('error', { 
+            title: 'Error',
+            nav: '1',
+            specificMessage: 'Missing Details.'
+        });
+    }
   }
   else {
-      res.render('error', { 
-          title: 'Error',
-          nav: '1',
-          specificMessage: 'Missing Details.'
-      });
+      res.render('index', { 
+      layout: path.join('layouts', 'with_jumbotron'),
+      title: 'Home',
+      nav: '1',
+      articles: await articleModel.find().lean(),
+      createArticle: true,
+      wrongSecretModify: true,
+      userInput: {
+        name: req.body.name,
+        description: req.body.description,
+        full: req.body.full
+      }
+   });
   }
 });
 
